442e09a39afc9dbf0ed07a1c0750fd3fe7fbecfb
Alex Roan <alex.roan@hotmail.com>
2020-02-14 13:36:16 +0000

    Updated interface to be more usable

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="container py-5">
 				<Content />
 			</div>
 		)
--- client/src/Content.js
+++ client/src/Content.js
@@ -14,16 +14,41 @@ const showForm = (props) => {
     }
 
     return (
-        <form className="row" onSubmit={submitForm}>
-            <div className="col-12 col-sm pr-sm-2">
-                <input type="number" 
-                    step="any"
-                    min="1"
-                    onChange={etherAmountChange}
-                    className="form-control form-control-sm bg-dark text-white"
-                    required />
-            </div>            
-        </form>
+        <div className="row justify-content-center">
+            <div className="col-8">
+                <div className="card">
+                    <div className="card-header">Double your money!</div>
+                    <div className="card-body">
+                        <form onSubmit={submitForm}>
+                            <div className="form-group row">
+                                <div className="col-md-3 text-md-right">
+                                    <label htmlFor="deposit" className="col-form-label">Investment (ETH)</label>
+                                </div>
+                                <div className="col-md-9">
+                                    <input type="number" 
+                                        step="any"
+                                        name="deposit"
+                                        id="deposit"
+                                        min="1"
+                                        onChange={etherAmountChange}
+                                        className="form-control form-control"
+                                        required />
+                                </div>
+                            </div>
+
+                            <div className="form-group row">
+                                <div className="col-md-9 offset-md-3">
+                                    <button type="submit" className="btn btn-primary">
+                                        INVEST
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
     )
 }
 
