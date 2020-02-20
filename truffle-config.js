e330b487f276e4d42b6b1e64ea5b054157dada1c
Alex Roan <alex.roan@hotmail.com>
2020-02-20 19:29:27 +0000

    Deployment to kovan

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,9 @@
+require("babel-register");
+require("babel-polyfill");
+require("dotenv").config();
 const path = require("path");
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKey = process.env.PRIVATE_KEY || "";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,11 +14,28 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "5777",
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          //private keys array
+          [privateKey],
+          //url to ethereum node
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
     }
   },
   compilers: {
     solc: {
-      version: "0.5.0"
+      version: "0.5.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
 };
