b159364b4eb51fbb887ab950ab0f09081d4c54e1
Alex Roan <alex.roan@hotmail.com>
2020-02-24 01:28:16 +0000

    Updates

---
+++ precache-manifest.30812a3bcb3541db3b7fbb782676d149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb552027015ab920f00dc502d5cc7a68",
+    "url": "/EthereumPonzi/index.html"
+  },
+  {
+    "revision": "ab6a8836a16e5851d3b9",
+    "url": "/EthereumPonzi/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "5a8b850cb64e56c56aea",
+    "url": "/EthereumPonzi/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ab6a8836a16e5851d3b9",
+    "url": "/EthereumPonzi/static/js/2.7a3ed84f.chunk.js"
+  },
+  {
+    "revision": "5a8b850cb64e56c56aea",
+    "url": "/EthereumPonzi/static/js/main.edceb9b9.chunk.js"
+  },
+  {
+    "revision": "c84613e3132cc4c74122",
+    "url": "/EthereumPonzi/static/js/runtime-main.947f76e4.js"
+  }
+]);
